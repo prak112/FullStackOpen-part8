4c4991357a47dd89c9dd049cae977d94cf7a9894
PrakGov <55034062+prak112@users.noreply.github.com>
2024-10-24 15:37:19 +0300

    feat: Add Mutation, Error Handling, Enum

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,32 +1,36 @@
 // Import necessary modules
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { GraphQLError } = require('graphql')
+const { v1: uuid } = require('uuid')
 
 // Sample data
 let persons = [
   {
-    name: "Arto Hellas",
+    name: "Madam Pikachu",
     phone: "040-123543",
     street: "Tapiolankatu 5 A",
     city: "Espoo",
     id: "3d594650-3436-11e9-bc57-8b80ba54c431"
   },
   {
-    name: "Matti Luukkainen",
+    name: "Bulbasaur Jr.",
     phone: "040-432342",
     street: "Malminkaari 10 A",
     city: "Helsinki",
     id: '3d599470-3436-11e9-bc57-8b80ba54c431'
   },
   {
-    name: "Venla Ruuska",
+    name: "Venasaur Jr.",
     street: "Nallemäentie 22 C",
     city: "Helsinki",
     id: '3d599471-3436-11e9-bc57-8b80ba54c431'
   },
 ]
 
-// Schema : define the structure of the query (data types and relationships)
+// Schema : define data types and relationships
+// Query : define what requests are valid
+// Mutation : define POST request to add contact
 const typeDefs = `
   type Address {
     street: String!
@@ -40,23 +44,48 @@ const typeDefs = `
     id: ID!
   }
 
+  enum YesNo {
+    YES
+    NO
+  }
+
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
+  }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
   }
 `
 
-// Resolvers : define the response for the queries
+
+// Resolvers : define response function/structure of Query
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if(!args.phone){
+        return persons
+      }
+      const byPhone = (person) => 
+        args.phone === 'YES' ? person.phone : !person.phone
+      return persons.filter(byPhone)
+    },
     findPerson: (root, args) =>
       persons.find(p => p.name === args.name)
   },
   /**
-   * self-defined resolver for Person type since data in the database is structured differently than in the schema
+   * DEBUGGED:
+   * ApolloServer defines default Resolver which is based on schema,
+   * Self-defined Resolver - if data in the database is structured differently than in the schema. 
+   * For ex. custom 'address' field in Person object
    */
   Person: {
     address: (root) => {
@@ -65,8 +94,27 @@ const resolvers = {
         city: root.city
       }
     }
+  },
+
+  Mutation: {
+    addPerson: (root, args) => {
+      if(persons.find(p => p.name === args.name)){
+        throw new GraphQLError('Contact already exists. Add a unique name.',{
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.name
+            }
+          }
+        )
+      }
+
+      const person = { ...args, id: uuid() }
+      persons.concat(person)
+      return person
+    } 
   }
 }
+
 
 // Apollo Server : Heart of the application
 const server = new ApolloServer({
@@ -74,7 +122,6 @@ const server = new ApolloServer({
   resolvers,
 })
 
-// Start the server
 startStandaloneServer(server, {
   listen: { port: 4000 },
 }).then(({ url }) => {
